bc2e57ff50f5c5c69cfd64e8720648d537d625a6
05AnshulThakur <98889318+05AnshulThakur@users.noreply.github.com>
2022-03-07 14:18:12 +0530

    Update activity3.h

--- Project/3_Implementation/inc/activity3.h
+++ Project/3_Implementation/inc/activity3.h
@@ -1,18 +1,14 @@
 #ifndef __ACTIVITY_3_H__
 #define __ACTIVITY_3_H__
 
-
-/**
- * Macro Definitions
- */
-#define PWM_20_PERCENT (205) /**< This is for Output PWM for 20% duty cycle */
-#define PWM_40_PERCENT (410) /**< This is for Output PWM for 40% duty cycle */
-#define PWM_70_PERCENT (717) /**< This is for Output PWM for 70% duty cycle */
-#define PWM_95_PERCENT (973) /**< This is for Output PWM for 95% duty cycle */
+#define PWM_20_PERCENT (205) /**< Output PWM for 20% duty cycle */
+#define PWM_40_PERCENT (410) /**< Output PWM for 40% duty cycle */
+#define PWM_70_PERCENT (717) /**< Output PWM for 70% duty cycle */
+#define PWM_95_PERCENT (973) /**< Output PWM for 95% duty cycle */
 
 /**
  * Include files
- */
+ */ 
 #include <util/delay.h>
 #include <avr/io.h>
 
@@ -21,12 +17,16 @@
  */
 
 /**
- * @brief Function to Initialize Timer1 registers
- *
+ * @brief Initialize Timer1 registers 
+ * 
  */
-void Init_Timer();
+void InitTimer();
 
+/**
+ * @brief Produce duty cycle in PWM according to i/p ADC value
+ * 
+ * @param[in] temp The ADC value from activity2
+ */
+void activity3_PWM(uint16_t temp);
 
-void PWM(uint16_t temp);
-
-#endif 
+#endif /* __ACTIVITY_3_H__ */
