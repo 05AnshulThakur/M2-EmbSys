66306cabea6f7bd8c3c2d970fa65fbcb0caf6de6
05AnshulThakur <98889318+05AnshulThakur@users.noreply.github.com>
2022-03-07 14:30:09 +0530

    Update activity4.c

--- Project/3_Implementation/src/activity4.c
+++ Project/3_Implementation/src/activity4.c
@@ -1,6 +1,16 @@
-#include "act4.h"
+/**
+ * @file activity4.c
+ * @author Anshul Thakur
+ * @brief Functions to print the Temperature in Oscilloscope 
+ * @date 2022-03-7
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
-void Init_USART()
+#include "activity4.h"
+
+void USARTInit()
 {
     UBRR0H = (uint8_t)(UBRR_VALUE>>8);
     UBRR0L = (uint8_t)UBRR_VALUE;
@@ -8,7 +18,7 @@ void Init_USART()
     UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 }
 
-int Read_USART()
+int USARTRead()
 {
     while(!(UCSR0A &(1<<RXC0)))
     {
@@ -17,15 +27,15 @@ int Read_USART()
     return UDR0;
 }
 
-void USARTWritecharacter(uint16_t temp)
-{
+void activity4_USARTWrite(uint16_t temp)
+{ 
     int i=0;
         if(temp>=0 && temp<=200){
             unsigned char data1[]="Temperature: 20 degree C\n";
             i=0;
             while(data1[i]!=0){
             while (!( UCSR0A & (1<<UDRE0)));  // Wait for empty transmit buffer
-            UDR0 = data1[i];                 // Put data into buffer, sends the data
+            UDR0 = data1[i];        // Put data into buffer, sends the data
             i++;
             }
             _delay_ms(2000);
@@ -35,27 +45,27 @@ void USARTWritecharacter(uint16_t temp)
             i=0;
             while(data2[i]!=0){
             while (!( UCSR0A & (1<<UDRE0)));  // Wait for empty transmit buffer
-            UDR0 = data2[i];                 // Put data into buffer, sends the data
+            UDR0 = data2[i];        // Put data into buffer, sends the data
             i++;
             }
             _delay_ms(2000);
         }
-        else if(temp>=510 && temp<=700){
+        else if(temp>=510 && temp<=700){        
              unsigned char data3[]="Temperature: 29 degree C\n";
             i=0;
             while(data3[i]!=0){
             while (!( UCSR0A & (1<<UDRE0)));  // Wait for empty transmit buffer
-            UDR0 = data3[i];                 // Put data into buffer, sends the data
+            UDR0 = data3[i];        // Put data into buffer, sends the data
             i++;
             }
             _delay_ms(2000);
         }
-        else if(temp>=710 && temp<=1024){
-            unsigned char data4[]="Temperature: 33 degree C\n";
+        else if(temp>=710 && temp<=1024){         
+            unsigned char data4[]="Temperature: 33 degree C\n";     
             i=0;
             while(data4[i]!=0){
             while (!( UCSR0A & (1<<UDRE0)));  // Wait for empty transmit buffer
-            UDR0 = data4[i];                 // Put data into buffer, sends the data
+            UDR0 = data4[i];        // Put data into buffer, sends the data
             i++;
             }
             _delay_ms(2000);
@@ -65,10 +75,11 @@ void USARTWritecharacter(uint16_t temp)
             i=0;
             while(data5[i]!=0){
             while (!( UCSR0A & (1<<UDRE0)));  // Wait for empty transmit buffer
-            UDR0 = data5[i];                 // Put data into buffer, sends the data
+            UDR0 = data5[i];        // Put data into buffer, sends the data
             i++;
             }
             _delay_ms(2000);
-
+            
         }
 }
+
